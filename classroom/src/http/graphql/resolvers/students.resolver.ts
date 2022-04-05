80d2fdff5af3727556b1bcb98d21f0a1b7d11d40
Renato D. Schneider <renatodaltiba@gmail.com>
2022-04-05 12:51:42 -0400

    chore(classroom): Adicionado crud básico: course, enrollment, student

---
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -0,0 +1,31 @@
+import { UseGuards } from '@nestjs/common';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { EnrollmentsService } from '../../../services/enrollments.service';
+import { StudentsService } from '../../../services/students.service';
+import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { AuthUser, currentUser } from '../../auth/current-user';
+import { Student } from '../models/student';
+
+@Resolver(() => Student)
+export class StudentsResolver {
+  constructor(
+    private studentsService: StudentsService,
+    private enrollmentServices: EnrollmentsService,
+  ) {}
+
+  @Query(() => Student)
+  @UseGuards(AuthorizationGuard)
+  me(@currentUser() user: AuthUser) {
+    return this.studentsService.getStudentByAuthUserId(user.sub);
+  }
+  @Query(() => [Student])
+  @UseGuards(AuthorizationGuard)
+  students() {
+    return this.studentsService.listAllStudents();
+  }
+
+  @ResolveField()
+  enrollments(@Parent() student: Student) {
+    return this.enrollmentServices.listEnrollmentByStudentId(student.id);
+  }
+}
